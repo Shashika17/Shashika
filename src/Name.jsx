519cfc371ff7663c0a256c3f313eb9d2ae27961b
Shashika17 <shashigakavinda@gmail.com>
2023-08-02 23:08:13 +0530

    manusha

--- src/Name.jsx
+++ src/Name.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import './dark-theme.css'
 export default function Namee() {
   return (
-   <div><body><h1>HI.....</h1></body></div>
+   <div><body>
+    <h1>Manushata kellek set wewaaa</h1>
+   </body></div>
   )
 }

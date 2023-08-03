fb89f7e67195f1c44b9944f883141a4835961a99
Shashika17 <shashigakavinda@gmail.com>
2023-08-03 21:49:38 +0530

    manu

--- src/Name.jsx
+++ src/Name.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import './dark-theme.css'
+import MyImage from './manu.jpeg'
 export default function Namee() {
   return (
    <div><body>
     <h1>Manushata kellek set wewaaa</h1>
+    <img src={MyImage} alt="My Image" />
    </body></div>
   )
 }
